124642e721b0548e4ed2ae476d84e5bea2b1b3b4
Christoph Berger <c.berger@tum.de>
2019-05-14 11:18:13 +0200

    added gpu mode tests for server

---
+++ server_tests.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Christoph Berger
+# Script for evaluation and bulk segmentation of Brain Tumor Scans
+# using the MICCAI BRATS algorithmic repository
+#
+# Please refer to README.md and LICENSE.md for further documentation
+# This software is not certified for clinical use.
+
+__version__ = '0.1'
+__author__ = 'Christoph Berger'
+
+from orchestra import Orchestra
+import os
+
+if __name__ == '__main__':
+    #config = os.path.abspath('config-tests.json')
+    config = os.path.abspath('config.json')
+    orchestra = Orchestra(config)
+    dir = '~/brats/brats18/ATW_1_gr-b17-f'
+    #status, container, client = orchestra.runDummyContainer()
+    status = orchestra.runContainer('mic-dkfz', dir)
+    print(status)
