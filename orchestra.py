887d134115ca65d4a444f4f6eac3a93a4b3c3f2e
Christoph Berger <c.berger@tum.de>
2019-05-30 13:56:01 +0200

    added run iterate method for directory searching

--- orchestra.py
+++ orchestra.py
@@ -15,6 +15,7 @@ from pprint import pprint
 import json
 import docker
 import os
+import errno
 #set id of gpu to use for computations
 gpu = "7" 
 
@@ -86,31 +87,56 @@ class Orchestra(object):
         return True
 
 
+    #def runIterate(self):
+    #    """ Loads the config file with a list of available containers
+    #    """
+    #    # processes the config details
+    #    for key, item in self.config.items():
+    #        img_id = self.config[key]['id']
+    #        # load the container command, default is empty
+    #        command = self.config[key]['command']
+    #        print(command)
+    #        print(img_id)
+    #        mount = '/data'
+    #        # distinguish between a batch container (iterates over all
+    #        # patients in the root directory by itself) and a standard
+    #        # container (needs to be called for each patient individually)
+    #        print('Total containers run: ', self.noOfContainers)
 
-    def runIterate(self):
-        """ Loads the config file with a list of available containers
+
+    def runIterate(self, dir, cid):
+        """ Iterates over a directory and runs the segmentation on each patient found
         """
-        # processes the config details
-        for key, item in self.config.items():
-            img_id = self.config[key]['id']
-            # load the container command, default is empty
-            command = self.config[key]['command']
-            print(command)
-            print(img_id)
-            mount = '/data'
-            # distinguish between a batch container (iterates over all
-            # patients in the root directory by itself) and a standard
-            # container (needs to be called for each patient individually)
-            print('Total containers run: ', self.noOfContainers)
+        print('Looking for BRATS data directories..')
+        for fn in os.listdir(dir):
+            if not os.path.isdir(os.path.join(dir, fn)):
+                continue # Not a directory
+            if 'DE_RI' in fn:
+                print('Found pat data: ', fn)
+                try:
+                    os.makedirs(os.path.join(os.path.join(dir, fn),
+                    'results'))
+                except OSError as err:
+                    if err.errno != errno.EEXIST:
+                        raise
+                print('Calling Container: ', cid)
+                if not self.runContainer('cid', os.path.join(dir, fn)):
+                    print('ERROR: Run failed for patient ', fn, ' with container', cid)
+                    return False
+                #TODO: rename folder and prepend pat_id
+                #rename_folder(img_id, os.path.join(directory, fn), fn)
+        return True
+        
+
 
 
 if __name__ == '__main__':
     #config = os.path.abspath('config-tests.json')
     config = os.path.abspath('config.json')
     orchestra = Orchestra(config)
-    dir = '/Users/christoph/Documents/Uni/HiWi/IBBM/Testdata/Brats17_CBICA_AAM_1'
+    dir = '/home/bergerc/bratum/Bene_Segmentation'
     #status, container, client = orchestra.runDummyContainer()
-    status = orchestra.runContainer('mic-dkfz', dir)
+    status = orchestra.runIterate(dir, 'mic-dkfz')
     if(status):
         print('Container run was succesful')
     else:
